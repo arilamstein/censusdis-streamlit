f51542d153a6dfb757fda7e1235efbe86ef885a4
Ari Lamstein <arilamstein@gmail.com>
2024-02-19 21:57:13 -0800

    Add alpha to map. Placeholder dropdown for variable selection.

--- streamlit-census-test.py
+++ streamlit-census-test.py
@@ -5,19 +5,12 @@ from censusdis.datasets import ACS5
 from censusdis import states
 
 @st.cache_data
-def get_median_income(state_fips):
+def get_census_data(state_fips, census_var):
  
     return ced.download(
-        # Data set: American Community Survey 5-Year
         dataset=ACS5,
-        
-        # Vintage: 2022
-        vintage=2022, 
-        
-        # Variable: median household income
-        download_variables=['NAME', 'B19013_001E'], 
-        
-        # Geography: All counties in New Jersey.
+        vintage=2022,         
+        download_variables=['NAME', census_var], 
         state=state_fips,
         county='*',
         with_geometry=True)
@@ -28,9 +21,18 @@ all_state_names = list(states.NAMES_FROM_IDS.values())
 state_name = st.selectbox("Select a State: ",all_state_names)
 state_fips = states.IDS_FROM_NAMES[state_name]
 
-df = get_median_income(state_fips)
-st.dataframe(df[['NAME', 'B19013_001E']], hide_index=True)
-fig = cem.plot_map(df, 'B19013_001E', legend=True, with_background=True)
+# all_census_vars = {'Median Household Income': 'B19013_001E',
+#                'Median Rent': 'B25058_E',
+#                'Median Age': 'B01002_E'}
+all_census_vars = {'Median Household Income': 'B19013_001E'}
+
+selected_census_var = st.selectbox("Select a demographic", all_census_vars.keys())
+census_var = all_census_vars[selected_census_var]
+
+df = get_census_data(state_fips, census_var)
+st.dataframe(df[['NAME', census_var]], hide_index=True)
+
+fig = cem.plot_map(df, 'B19013_001E', legend=True, with_background=True, alpha=.5)
 st.pyplot(fig.figure)
 
-st.write("[github](https://github.com/arilamstein/censusdis-streamlit)")
+st.write("View the code [here](https://github.com/arilamstein/censusdis-streamlit).")

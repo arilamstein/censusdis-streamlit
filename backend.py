a63d821850263c29ab0648557f6c2b88eeff1343
Ari Lamstein <arilamstein@gmail.com>
2024-04-20 11:20:18 -0700

    First step in moving analysis to county-based time series for all available data (2005-2022, excluding 2020).

--- backend.py
+++ backend.py
@@ -29,7 +29,9 @@ census_vars = {
     # var_label                var_table
     'Median Household Income': 'B19013_001E',
     'Total Population'       : 'B01001_001E',
-    'Median Rent'            : 'B25058_001E' }
+    'Median Rent'            : 'B25058_001E' 
+    # TODO: Maybe add B08006_017E - Work from Home?
+}
 
 census_vars_reverse = {v: k for k, v in census_vars.items()}
 
--- gen_all_county_data.py
+++ gen_all_county_data.py
@@ -1,15 +1,16 @@
 import pandas as pd
+import time
 from backend import census_vars
-
 import censusdis.data as ced
 from censusdis.datasets import ACS1
 from censusdis.states import ALL_STATES_AND_DC
 
+start_time = time.time()
 df_all = None
 
-# Skip 2020 for now because data appears to be missing! 
+# Skip 2020 because it was not published due to covid.
 # See https://www.census.gov/programs-surveys/acs/data/experimental-data.html
-years =[2018, 2019, 2021, 2022]
+years =[2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021, 2022]
 for one_year in years: 
     df_new = ced.download(
         dataset=ACS1,
@@ -27,3 +28,6 @@ for one_year in years:
         df_all = pd.concat([df_all, df_new])
 
 df_all.to_csv('county_data_for_all_years.csv')
+
+# Took me about 1 minute
+print(f"Script gen_all_county_data.py finished in {time.time() - start_time}")
